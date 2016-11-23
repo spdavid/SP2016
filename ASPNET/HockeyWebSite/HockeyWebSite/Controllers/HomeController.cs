41adee3bc2a7c427383ad78c47bbbf8aa5226a50
SPDavid <david@zalo.se>
2016-11-23 10:12:06 +0100

    commit

--- ASPNET/HockeyWebSite/HockeyWebSite/Controllers/HomeController.cs
+++ ASPNET/HockeyWebSite/HockeyWebSite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HockeyWebSite.Models;
 
 namespace HockeyWebSite.Controllers
 {
@@ -11,7 +12,26 @@ namespace HockeyWebSite.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            HockeyEntities db = new HockeyEntities();
+
+            return View(db.Teams.ToList());
+
+
+        }
+
+        public ActionResult TeamPlayers(int? TeamId)
+        {
+            if (TeamId == null)
+            {
+                return HttpNotFound();
+            }
+          //int TeamId =  int.Parse(Request.QueryString["TeamId"])
+            HockeyEntities db = new HockeyEntities();
+            Team team = db.Teams.Where(t => t.Id == TeamId.Value).FirstOrDefault();
+
+            return View(team);
+
+
         }
     }
 }
