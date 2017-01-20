f5616e0a565e521e46e193015e47e3d0d850b0f3
SPDavid <david@zalo.se>
2017-01-20 13:47:52 +0100

    lists views and more

--- OfficeDev1/MyFirstCSOMApp/MyFirstCSOMApp/CSOM/BooksAssignment.cs
+++ OfficeDev1/MyFirstCSOMApp/MyFirstCSOMApp/CSOM/BooksAssignment.cs
@@ -16,7 +16,41 @@ namespace MyFirstCSOMApp.CSOM
             string pathToXML = AppDomain.CurrentDomain.BaseDirectory + "BooksCtFields.xml";
             root.CreateFieldsFromXMLFile(pathToXML);
             root.CreateContentTypeFromXMLFile(pathToXML);
+            List booksList = null;
+            if (!root.ListExists("Books"))
+            {
+                booksList = root.CreateList(ListTemplateType.GenericList, "Books", false, true, "lists/books", true);
+            }
+            else
+            {
+                booksList = root.GetListByTitle("Books");
+            }
 
+            // if you need to enable content types
+            //List booksList = root.GetListByTitle("Books");
+            //booksList.ContentTypesEnabled = true;
+            //booksList.Update();
+            //ctx.ExecuteQuery();
+
+            if (!booksList.ContentTypeExistsByName("Book"))
+            {
+                booksList.AddContentTypeToListByName("Book", true);
+                booksList.RemoveContentTypeByName("Item");
+
+                View view = booksList.DefaultView;
+                ctx.Load(view.ViewFields);
+                ctx.ExecuteQuery();
+
+                view.ViewFields.Add("OD1_BookType"); // internal name of field. 
+                view.ViewFields.Add("OD1_Author");
+                view.ViewFields.Add("OD2_ReleaseDate");
+                view.ViewFields.Add("OD1_Description");
+
+                view.Update();
+                ctx.ExecuteQuery();
+
+            }
+            
         }
     }
 }
