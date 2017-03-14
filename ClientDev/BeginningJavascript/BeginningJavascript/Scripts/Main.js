f7734b6555f9037c6a1abde2cafb3749c46d3f82
SPDavid <david@zalo.se>
2017-03-14 10:37:49 +0100

    Beginning Javascript

---
+++ ClientDev/BeginningJavascript/BeginningJavascript/Scripts/Main.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    // wait for page is loaded before running your code. 
+    //window.onload = function () {
+    //    var mydiv = document.getElementById("myDiv");
+    //    console.log(mydiv);
+    //    mydiv.innerText = "Hello World";
+    //}
+
+    // better way to wait until page loading.
+    ready(
+        function () {
+            var mydiv = document.getElementById("myDiv");
+            console.log(mydiv);
+            mydiv.innerText = "Hello World";
+
+            var someText = document.getElementById("sometext");
+            someText.value = "Hello World";
+
+            var mybutton2 = document.getElementById("mybutton2");
+            mybutton2.addEventListener("click",  ButtonClick);
+
+        }
+    );
+
+    function ButtonClick() {
+        var someText = document.getElementById("sometext");
+        someText.value = "Hello From Button";
+    }
+    window.ButtonClick = ButtonClick;
+
+
+
+    function ready(fn) {
+        if (document.readyState != 'loading') {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+
+})();
