12985c99b0ac65858c90a466b0719d73d4073aa3
SPDavid <david@zalo.se>
2017-09-06 13:04:56 +0200

    commit

---
+++ OfficeDev2/AppOnlyADContext/AppOnlyADContext/ContextHelper.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOnlyADContext
+{
+    public class ContextHelper
+    {
+
+        public static string EncryptSecret { get; set; }
+
+        //the method that will be provided to the KeyVaultClient
+        public static async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(ConfigurationManager.AppSettings["ClientId"],
+                        ConfigurationManager.AppSettings["ClientSecret"]);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
+        }
+
+
+        public static async Task<ClientContext> GetSPContext(string siteUrl)
+        {
+            // I put my GetToken method in a Utils class. Change for wherever you placed your method.
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(ContextHelper.GetToken));
+
+            var sec = await kv.GetSecretAsync(ConfigurationManager.AppSettings["SecretUri"]);
+
+
+
+            //I put a variable in a Utils class to hold the secret for general  application use.
+            ContextHelper.EncryptSecret = sec.Value;
+
+
+            var exportedCertCollection = new X509Certificate2Collection();
+            exportedCertCollection.Import(Convert.FromBase64String(ContextHelper.EncryptSecret));
+            var cert2 = exportedCertCollection.Cast<X509Certificate2>().Single(s => s.HasPrivateKey);
+
+
+            AuthenticationManager authmanager = new AuthenticationManager();
+
+            var ctx = authmanager.GetAzureADAppOnlyAuthenticatedContext(siteUrl, ConfigurationManager.AppSettings["ClientId"], ConfigurationManager.AppSettings["tenant"], cert2);
+            return ctx;
+        }
+    }
+}
---
+++ OfficeDev2/AppOnlyADContext/AppOnlyADContext/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOnlyADContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext ctx = ContextHelper.GetSPContext("https://folkuniversitetetsp2016.sharepoint.com/").Result;
+
+
+            List list = ctx.Web.GetListByTitle("CustomList");
+
+            string currentChangeTokenstr = list.GetPropertyBagValueString("customlistchangetoken", null);
+            ChangeToken currentChangeToken = null;
+            if (currentChangeTokenstr != null)
+            {
+                currentChangeToken = new ChangeToken();
+
+                currentChangeToken.StringValue = currentChangeTokenstr;
+            }
+
+            ChangeQuery q = new ChangeQuery(false, false);
+            q.Update = true;
+            q.Add = true;
+            q.Item = true;
+            q.DeleteObject = true;
+            q.ChangeTokenStart = currentChangeToken;
+            q.ChangeTokenEnd = list.CurrentChangeToken;
+
+            ChangeCollection changes = list.GetChanges(q);
+            ctx.Load(changes);
+            ctx.ExecuteQueryRetry();
+
+            foreach (var change in changes)
+            {
+                if (change.ChangeType == ChangeType.Add)
+                {
+                    ChangeItem itemChange = change as ChangeItem;
+                    Console.WriteLine("Item was added id = " + itemChange.ItemId);
+
+                }
+                if (change.ChangeType == ChangeType.DeleteObject)
+                {
+                    ChangeItem itemChange = change as ChangeItem;
+                    Console.WriteLine("Item was deleted id = " + itemChange.ItemId);
+                }
+                if (change.ChangeType == ChangeType.Update)
+                {
+                    ChangeItem itemChange = change as ChangeItem;
+                    Console.WriteLine("Item was updated id = " + itemChange.ItemId);
+                }
+            }
+            list.SetPropertyBagValue("customlistchangetoken", q.ChangeTokenEnd.StringValue);
+
+
+
+
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+
+        }
+    }
+}
