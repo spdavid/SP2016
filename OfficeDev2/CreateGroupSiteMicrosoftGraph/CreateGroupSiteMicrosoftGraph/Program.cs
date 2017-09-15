40ff802111a08843f3f6c440547795190d381b5f
SPDavid <david@zalo.se>
2017-09-15 10:43:51 +0200

    Create a group with owner

---
+++ OfficeDev2/CreateGroupSiteMicrosoftGraph/CreateGroupSiteMicrosoftGraph/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateGroupSiteMicrosoftGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         Helpers.GroupHelper.CreateUnifedGroup("davidtest6", "davidtest6", "davidtest6", "David@folkuniversitetetsp2016.onmicrosoft.com");
+
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+    }
+}
